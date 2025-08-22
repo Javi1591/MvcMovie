2e6882aef3b8ba228f9034fb6e3a4ccb8c3adff2
Javi1591 <160084774+Javi1591@users.noreply.github.com>
2025-08-22 19:22:09 -0400

    Added logging using Ilogger.Update README.md

--- MvcMovie/Controllers/MoviesController.cs
+++ MvcMovie/Controllers/MoviesController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MvcMovie.Data;
 using MvcMovie.Models;
+using Microsoft.Extensions.Logging;
 
 namespace MvcMovie.Controllers
 {
@@ -14,16 +15,27 @@ namespace MvcMovie.Controllers
     {
         private readonly MvcMovieContext _context;
 
-        public MoviesController(MvcMovieContext context)
+        // Implement Basic Logging
+        // Use ILogger
+        private readonly ILogger<MoviesController> _logger;
+
+        public MoviesController(MvcMovieContext context, ILogger<MoviesController> logger)
         {
             _context = context;
+            _logger = logger;
         }
 
         // GET: Movies
         public async Task<IActionResult> Index(string movieGenre, string searchString)
         {
+            // Log getting db
+            _logger.LogDebug("Searched for {searchString}", searchString);
+
             if (_context.Movie == null)
             { 
+                // Log any nulls when getting db
+                _logger.LogError("Object is null at {Time}",DateTimeOffset.UtcNow);
+
                 return Problem("Entity set 'MvcMovieContext.Movie' is null.");
             }
 
@@ -37,12 +49,16 @@ namespace MvcMovie.Controllers
 
             if (!String.IsNullOrEmpty(searchString))
             {
+                // Log filter information
                 movies = movies.Where(s => s.Title!.ToUpper().Contains(searchString.ToUpper()));
+                _logger.LogDebug("Applied filter");
             }
 
             if (!string.IsNullOrEmpty(movieGenre))
             {
+                // Log filters for movieGenre
                 movies = movies.Where(x => x.Genre == movieGenre);
+                _logger.LogDebug("Applied filter by {movieGenre}.", movieGenre);
             }
 
             var movieGenreVM = new MovieGenreViewModel
@@ -50,6 +66,9 @@ namespace MvcMovie.Controllers
                 Genres = new SelectList(await genreQuery.Distinct().ToListAsync()),
                 Movies = await movies.ToListAsync()
             };
+
+            // Log getting movieGenreVM
+            _logger.LogDebug("Returned {movieGenreVM}", movieGenreVM);
 
             return View(movieGenreVM);
         }
@@ -59,15 +78,24 @@ namespace MvcMovie.Controllers
         {
             if (id == null)
             {
+                // Log any null Details
+                _logger.LogWarning("Movie with {id} not found.", DateTimeOffset.UtcNow);
+
                 return NotFound();
             }
 
-            var movie = await _context.Movie
-                .FirstOrDefaultAsync(m => m.Id == id);
+            var movie = await _context.Movie.FirstOrDefaultAsync(m => m.Id == id);
+
             if (movie == null)
             {
+                // Log any null movies
+                _logger.LogWarning("Movie {id} has null values", id);
+
                 return NotFound();
             }
+
+            // Log found movies
+            _logger.LogDebug("Found movie with {id}", id);
 
             return View(movie);
         }
@@ -89,8 +117,16 @@ namespace MvcMovie.Controllers
             {
                 _context.Add(movie);
                 await _context.SaveChangesAsync();
+
+                // Log posting of new movie
+                _logger.LogDebug("{movie} has been created successfully", movie);
+
                 return RedirectToAction(nameof(Index));
             }
+
+            // Log any errors for new create movie
+            _logger.LogDebug("{movie} was NOT created, please see errors.", movie);
+
             return View(movie);
         }
 
@@ -99,14 +135,24 @@ namespace MvcMovie.Controllers
         {
             if (id == null)
             {
+                // Log any movies not found with id
+                _logger.LogWarning("You can NOT edit the movie with {id} because it is not found", id);
+
                 return NotFound();
             }
 
             var movie = await _context.Movie.FindAsync(id);
+
             if (movie == null)
             {
+                // Log any movies with null titles
+                _logger.LogWarning("Movie with {title} is null and can NOT be editted at this time. Please try again later.", movie);
+
                 return NotFound();
             }
+            // Log editted movie
+            _logger.LogDebug("{movie} has been successfully editted.", movie);
+
             return View(movie);
         }
 
@@ -126,13 +172,22 @@ namespace MvcMovie.Controllers
             {
                 try
                 {
+                    // Log postting of movie edits
+                    _logger.LogDebug("{movie} with {id} is being editted.", movie, id);
+
                     _context.Update(movie);
                     await _context.SaveChangesAsync();
+
+                    // Log postting of successful edits
+                    _logger.LogDebug("{movie} with {id} was successsfully editted", movie, id);
                 }
                 catch (DbUpdateConcurrencyException)
                 {
                     if (!MovieExists(movie.Id))
                     {
+                        // Log any movie id that does NOT exist
+                        _logger.LogError("{movie} with {id} does NOT exist, please try again.", movie, id);
+
                         return NotFound();
                     }
                     else
@@ -142,6 +197,10 @@ namespace MvcMovie.Controllers
                 }
                 return RedirectToAction(nameof(Index));
             }
+
+            // Log any unsuccessful edits
+            _logger.LogError("{movie} with {id} was not editted, please try again later.", movie, id);
+
             return View(movie);
         }
 
@@ -160,6 +219,7 @@ namespace MvcMovie.Controllers
                 return NotFound();
             }
 
+            // Return strongly typed view
             return View(movie);
         }
 
@@ -171,6 +231,8 @@ namespace MvcMovie.Controllers
             var movie = await _context.Movie.FindAsync(id);
             if (movie != null)
             {
+                // Log postting of deleting movie
+                _logger.LogDebug("{movie} with {id} was deleted.", movie, id);
                 _context.Movie.Remove(movie);
             }
 
--- README.md
+++ README.md
@@ -5,8 +5,8 @@
 This is a web application that shows a list of movies including the title, genre, release date, price, and parental guidance rating. It features include allowing a user to creating, editing, and deleting entries into the list. Users are also able to search for movies by filtering through genre. 
 
 # How to run the application locally
-Step 1: Clone the repository by navitgating to https://github.com/Javi1591/MvcMovie
-Step 2: Navigate to the project directory, using comman "cd MvcMovie" in the command console
+Step 1: Clone the repository by navitgating to http://github.com/Javi1591/MvcMovie
+Step 2: Navigate to the project directory, using command "cd MvcMovie" in the command console
 Step 3: Install the needed dependencies, as noted below, using the command "npm install"
   Needed dependencies for the MvcMovie application to successfully run include:
     Packages -
